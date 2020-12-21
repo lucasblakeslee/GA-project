fab3d1852bd35bd594f7d787d1283482785f307d
lucas blakeslee <lqblakeslee@gmail.com>
2020-12-21 12:30:54 -0700

    cleaned up the code a little: removed some unnecessary comments

--- GA-poly-max.py
+++ GA-poly-max.py
@@ -1,47 +1,18 @@
 #! /usr/bin/env python3
 
-##  g e n e r a l   f l o w
-#
-#population
-#v
-#fitness calculation
-#v
-#mating pool
-#v
-#parents selection
-#v
-#mating - crossover
-#       - mutation
-#v
-#offspring
-#v
-#>->->->->->->->->->->-> population
-
-
-#######################
-#
-#goal is to maximize this
-#    y = a1x1+a2x2+a3x3+a4x4+a5x5+a6x6
-#   (x1,x2,x3,x4,x5,x6)=(2.8,-4.2,3.8,5.6,-11,-6.8) (arbitrary values)
-#    What are the best values for a1-a6
-#
-#######################
-
 import numpy
 import random
 
-######################
+#
 
-
-################################################################
 # Inputs
 poly_coefs = [2.8, 1, 1, -3.6, 3.8, 1.6, -0.3]
 
 # Number of the polynomial coefs we are looking to optimize.
 n_poly_coefs = len(poly_coefs)
 
-####################
 #
+
 #mating pool size and population size
 
 n_pop = 8                       # number of chromosomes, or number of population members
@@ -55,65 +26,40 @@ best_outputs = []
 num_generations = 1000
 
 def main():
-    new_population = numpy.random.uniform(low=-1000.0, high=1000.0, 
-                                          size=n_pop)
+    new_population = numpy.random.uniform(low=-1000.0, high=1000.0, size=n_pop)
     print(new_population)
     for generation in range(num_generations):
-        print("Generation : ", generation)
+        print("Generation: ", generation)
         # Calculates fitness for each person and put them in a list of
         # fitnesses
         fit_list = []
         for dude in new_population:
             dude_fitness = calc_fitness(dude)
             fit_list.append(dude_fitness)
-        print("Fitness")
+        print("Fitness:")
         print(fit_list)
         
-        # best_outputs.append(numpy.max(numpy.sum(new_population*equation_inputs, axis=1)))
         # The best result in the current iteration.
         max_index = numpy.argmax(fit_list)
         max_dude = new_population[max_index]
         max_fit = fit_list[max_index]
-        print("Best result : ", max_index, max_dude, max_fit)
+        print("Best result: ", max_index, max_dude, max_fit)
 
         # Selecting the best parents in the population for mating.
         new_population = select_pool(new_population, fit_list,
                                      num_parents_mating)
-        # print("Parents")
-        # print(parents)
 
-        # # Generating next generation using crossover.
-        # offspring_crossover = crossover(parents, offspring_size=(pop_size[0]-parents.shape[0], num_weights))
-        # print("Crossover")
-        # print(offspring_crossover)
 
-        # # Adding variations to the offspring using mutation.
-        # offspring_mutation = mutation(offspring_crossover, num_mutations=2)
-        # print("Mutation")
-        # print(offspring_mutation)
-
-        # # new population based on the parents and offspring.
-        # new_population[0:parents.shape[0], :] = parents
-        # new_population[parents.shape[0]:, :] = offspring_mutation
-
-        # # Getting the best solution after iterating all generations.
-        # #fitness is calculated for each solution in the final generation.
-        # fitness = cal_fitness(equation_inputs, new_population)
-        # # index of solution corresponding to the best fitness.
-        # best_match_idx = numpy.where(fitness == numpy.max(fitness))
-
-        # print("Best solution : ", new_population[best_match_idx, :])
-        # print("Best solution fitness : ", fitness[best_match_idx])
+        print('---------------------------')
         print('==== new_pop ====')
         print(new_population)
+        print(' ')
         print('max_ind_fit:', max_dude, '   ', max_fit)
 
-
+#
 
 def calc_fitness(x):
-    #Fitness value of each solution
     #Sum of products between each input and corresponding weight.
-    # fitness = numpy.sum(pop*equation_inputs, axis=1)
     # return fitness
     # evaluate the polynomial at the given point
     fit = 0
@@ -131,9 +77,10 @@ def select_pool(pop, fit_list, num_parents_mating):
     top_half_parent_indices = sorted_parent_indices[-num_parents_mating:]
     top_half_parents = pop[[top_half_parent_indices]]
     print('top_half_parents:', top_half_parents)
+    print(' ')
     child_pop = []
     for i in range(num_parents_mating // 2):
-        print('i:', i, num_parents_mating)
+#        print('i:', i, num_parents_mating)
         p1 = top_half_parents[2*i]
         p2 = top_half_parents[2*i + 1]
         c1, c2 = mate(p1, p2)
