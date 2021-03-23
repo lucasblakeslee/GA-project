d3249e6cf0c08cc0e08fb61fef6f29cde47daca4
Mark Galassi <mark@galassi.org>
2021-03-22 22:33:22 -0600

    2021-03-22  Mark Galassi  <mark@galassi.org>
    	* plot_ga_output.py: added a center panel which shows the log
    	scale of the left panel plots.

--- plot_ga_output.py
+++ plot_ga_output.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import math
 import sys
 import glob
 import numpy as np
@@ -17,13 +18,9 @@ def main():
     else:
         flist = sorted(glob.glob('GA-gen-info_pid*.out'))
     for runfile in flist:
-        fig, axtxt, axs = prepare_plots(runfile)
+        fig, axtxt, axs, axslog = prepare_plots(runfile)
         present_metadata(runfile, axtxt)
-        plot_runfile(runfile, fig, axs)
-        # t = "This is a really long string that I'd rather have wrapped so that it"\
-        #     " doesn't go outside of the figure, but if it's long enough it will go"\
-        #     " off the top or bottom!"
-        # axtxt.text(0, 0, t, ha='left', rotation=0, wrap=True)
+        plot_runfile(runfile, fig, axs, axslog)
     plt.show()
 
 def present_metadata(fname, ax):
@@ -40,32 +37,46 @@ def present_metadata(fname, ax):
 
 def prepare_plots(fname):
     # fig = plt.figure(constrained_layout = True)
-    fig = plt.figure()
-    gs = gridspec.GridSpec(3, 2)
+    fig = plt.figure(figsize=(9.6, 5.0))
+    gs = gridspec.GridSpec(3, 3)
     axtxt = fig.add_subplot(gs[:, -1])
     axtxt.xaxis.set_visible(False)
     axtxt.yaxis.set_visible(False)
     axs = [fig.add_subplot(gs[i, 0]) for i in range(3)]
+    axslog = [fig.add_subplot(gs[i, 1]) for i in range(3)]
     suptitle = fig.suptitle(f'GA output file {fname}', fontsize='x-large')
     suptitle.set_y(0.98)
-    return fig, axtxt, axs
+    return fig, axtxt, axs, axslog
     
 
-def plot_runfile(fname, fig, axs):
+def plot_runfile(fname, fig, axs, axslog):
     print(f'# plotting GA run from file {fname}')
     gens, fittest, max_fit, avg_fit, elite_avg_fit, entropy = load_file(fname)
     axs[0].plot(gens, max_fit, gens, avg_fit, gens, elite_avg_fit)
     axs[0].set_xlim(0, gens.size - 1)
     axs[0].set_xlabel('generation')
     axs[0].set_ylabel('fitness (max,avg,elite)')
+    axslog[0].set_yscale('log')
+    axslog[0].plot(gens, max_fit, gens, avg_fit, gens, elite_avg_fit)
+    axslog[0].set_xlim(0, gens.size - 1)
+    axslog[0].set_xlabel('generation')
+    axslog[0].set_ylabel('fitness (max,avg,elite)')
 
     axs[1].plot(gens, entropy)
     axs[1].set_xlim(0, gens.size - 1)
     axs[1].set_ylabel('entropy')
+    axslog[1].set_yscale('log')
+    axslog[1].plot(gens, entropy)
+    axslog[1].set_xlim(0, gens.size - 1)
+    axslog[1].set_ylabel('entropy')
 
     axs[2].plot(gens, fittest)
     axs[2].set_xlim(0, gens.size - 1)
     axs[2].set_ylabel('position')
+    axslog[2].set_yscale('log')
+    axslog[2].plot(gens, fittest)
+    axslog[2].set_xlim(0, gens.size - 1)
+    axslog[2].set_ylabel('position')
 
 
     fig.subplots_adjust(top=0.65)
